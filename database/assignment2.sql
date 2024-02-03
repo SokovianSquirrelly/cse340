f239a116f4c367cfef1461ad4857134cc2ecae45
Collin Squires <squirrelly392@gmail.com>
2024-02-03 12:45:11 -0700

    added a database to project

---
+++ database/assignment2.sql
@@ -0,0 +1,25 @@
+INSERT INTO account
+	(account_firstname, account_lastname, account_email, account_password)
+VALUES
+	('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+UPDATE account
+SET account_type='Admin' account_firstname='Tony' AND account_lastname='Stark';
+
+DELETE FROM account
+WHERE account_firstname='Tony' AND account_lastname='Stark';
+
+UPDATE inventory 
+SET inv_description = 
+REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_make='GM' AND inv_model='Hummer';
+
+SELECT i.inv_make, i.inv_model, c.classification_name 
+FROM inventory i 
+INNER JOIN classification c 
+USING (classification_id)
+WHERE classification_name='Sport';
+
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, 'images/', 'images/vehicles/'),
+inv_thumbnail = REPLACE(inv_thumbnail, 'images/', 'images/vehicles/');
