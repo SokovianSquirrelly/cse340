c6a7e852c256c13cb84cee8a40a0f997c498f669
Collin Squires <squirrelly392@gmail.com>
2024-02-15 08:30:52 -0700

    added error handling

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const static = require("./routes/static");
 const expressLayouts = require("express-ejs-layouts");
 const baseController = require("./controllers/base-controller");
 const inventoryRoute = require("./routes/inventoryRoute.js");
+const utilities = require("./utilities/index.js");
 
 /* ***********************
  * View Engine and Templates
@@ -25,9 +26,31 @@ app.set("layout", "./layouts/layout");
  * Routes
  *************************/
 app.use(static)
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page..." });
+});
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
