4a2a2f82d35ac6f350fd52145359677d5fe3a9ae
Collin Squires <squirrelly392@gmail.com>
2024-02-26 08:00:43 -0700

    attempted to get registration working

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,4 +7,6 @@ router.get("/login", utilities.handleErrors(accController.buildLogin));
 
 router.get("/register", utilities.handleErrors(accController.buildRegister));
 
+router.post("/register", utilities.handleErrors(accController.registerAccount));
+
 module.exports = router;
