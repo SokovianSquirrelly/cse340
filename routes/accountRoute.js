7509c09e42788ee79c691bfa6d58df6a422ee899
Collin Squires <squirrelly392@gmail.com>
2024-02-27 15:25:17 -0700

    added server-side validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = new express.Router();
 const accController = require("../controllers/acc-controller");
 const utilities = require("../utilities");
+const regValidate = require("../utilities/account-validation");
 
 router.get("/login", utilities.handleErrors(accController.buildLogin));
 
 router.get("/register", utilities.handleErrors(accController.buildRegister));
 
-router.post("/register", utilities.handleErrors(accController.registerAccount));
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accController.registerAccount)
+);
 
 module.exports = router;
